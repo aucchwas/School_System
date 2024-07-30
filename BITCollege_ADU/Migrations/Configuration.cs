5dd398d30d667fa02daf073dc1c2b0bd969d1875
aucchwas <ucchwasa@gmail.com>
2024-07-29 21:45:07 -0500

    The whole project

---
+++ BITCollege_ADU/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace BITCollege_ADU.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BITCollege_ADU.Data.BITCollege_ADUContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BITCollege_ADU.Data.BITCollege_ADUContext";
+        }
+
+        protected override void Seed(BITCollege_ADU.Data.BITCollege_ADUContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
